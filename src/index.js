ca97684d5d05e7540c3e4dcc68f7d01a74f4b6ab
Talon Revis <113617774+beaconweb3@users.noreply.github.com>
2023-04-11 19:30:00 -0500

    Completed all the sections of the assignments using loops and functions

--- src/index.js
+++ src/index.js
@@ -40,3 +40,46 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
 };
 
 console.log('project wired!')
+
+//Anchor Tags and DImages
+const anchorTags = document.querySelectorAll("a");
+  //set class name
+anchorTags.forEach(a => a.className = "italic");
+  //set anchor textContent
+for (let i = 0; i<anchorTags.length-1; i++) {
+  anchorTags[i].textContent = siteContent['nav'][`nav-item-${i+1}`];
+}
+anchorTags[anchorTags.length-1].textContent = siteContent['footer']['copyright'];
+anchorTags[anchorTags.length-1].className = 'bold';
+//All Images
+const img = document.querySelectorAll('img');
+img[0].setAttribute('src', siteContent["images"]["logo-img"]);
+img[1].setAttribute('src', siteContent["images"]["cta-img"]);
+img[2].setAttribute('src', siteContent["images"]["accent-img"]);
+
+//CTA Section - Header and Button
+const h1 = document.querySelector('h1');
+h1.textContent = siteContent['cta']['h1'];
+document.querySelector('button').textContent = siteContent['cta']['button'];
+
+//Main-Content Section 
+  //h4 & p
+  const h4 = document.querySelectorAll('h4');
+  const p = document.querySelectorAll('p');
+  let a = 0; let b = 1; 
+    for(let i = 0; i<h4.length-1;i++){
+    h4[i].textContent = siteContent['main-content'][Object.keys(siteContent['main-content'])[a]];
+    p[i].textContent = siteContent['main-content'][Object.keys(siteContent['main-content'])[b]];
+    a+=2; b+=2;
+    }
+
+//Contact Section
+h4[5].textContent = siteContent['contact']['contact-h4'];
+let c = 1;
+for(let i = 5; i<p.length; i++){
+  p[i].textContent = siteContent['contact'][Object.keys(siteContent['contact'])[c]];
+  c++;
+}
+
+//
+
